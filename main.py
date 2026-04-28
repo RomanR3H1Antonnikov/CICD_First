41d83eaf8857d22d1b615413113b2c9f8a3f5fd9
Роман <roman.v.antonnikov@gmail.com>
2026-04-28 20:41:56 +0300

    ДЗ: Настройка GithubActions 2

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 import requests
 
+
 def get_weather(city=""):
-    url = f"https://wttr.in/{city}?m"
+	url = f"https://wttr.in/{city}?m"
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
+	try:
+		response = requests.get(url)
+		response.raise_for_status()
 
-        print(f"Погода сегодня в {city if city else "вашем городе"}: {response.text}")
+		print(
+			f"Погода сегодня"
+			f" в {city if city else 'вашем городе'}: {response.text}"
+		)
 
-    except requests.exceptions.RequestException as e:
-        print(f"Ошибка при запросе погоды: {e}")
+	except requests.exceptions.RequestException as e:
+		print(f"Ошибка при запросе погоды: {e}")
 
 
 if __name__ == "__main__":
-    print("""Добрый день! Готовы узнать прогноз погоды? Доступные команды:
+	print("""Добрый день! Готовы узнать прогноз погоды? Доступные команды:
     1. Нажмите Enter - погода для вашего местоположения
     2. Введите название города - погода для указанного города
     3. Введите 'exit' - выход из программы""")
 
-    while True:
-        city = input("Введите город (или нажмите Enter): ").strip()
+	while True:
+		city = input("Введите город (или нажмите Enter): ").strip()
 
-        if city.lower() == 'exit':
-            print("Хорошего дня!")
-            break
-        elif city:
-            get_weather(city)
-        else:
-            get_weather()
+		if city.lower() == "exit":
+			print("Хорошего дня!")
+			break
+		elif city:
+			get_weather(city)
+		else:
+			get_weather()
 
-        print()
+		print()
