e2976e4753482f4fb2d73b6e6e6b15e452a98a86
Роман <roman.v.antonnikov@gmail.com>
2026-04-28 15:51:25 +0300

    Усовершенствовал код, ДЗ: Запуск скриптов выполнено успешно

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import requests
 
-def get_weather():
-    url = "https://wttr.in"
+def get_weather(city=None):
+    if city:
+        url = f"https://wttr.in/{city}?m"
+    else:
+        url = "https://wttr.in/?m"
+
     try:
         response = requests.get(url)
         response.raise_for_status()
 
-        print(f"Погода сегодня: {response.text}")
+        print(f"Погода сегодня в {city if city else "вашем городе"}: {response.text}")
 
     except requests.exceptions.RequestException as e:
         print(f"Ошибка при запросе погоды: {e}")
 
 
 if __name__ == "__main__":
-    get_weather()
+    print("Добрый день! Готовы узнать прогноз погоды? Доступные команды:")
+    print()
+    print("1. Нажмите Enter - погода для вашего местоположения")
+    print("2. Введите название города - погода для указанного города")
+    print("3. Введите 'exit' - выход из программы")
+    print()
+
+    while True:
+        city = input("Введите город (или нажмите Enter): ").strip()
+
+        if city.lower() == 'exit':
+            print("Хорошего дня!")
+            break
+        elif city:
+            get_weather(city)
+        else:
+            get_weather()
+
+        print()
