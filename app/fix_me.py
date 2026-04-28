8046038c963fd4dd43d4ad8a8ea436df4a7564cf
Роман <roman.v.antonnikov@gmail.com>
2026-04-28 17:06:27 +0300

    Доработал pyproject.toml для ДЗ: Исправление ошибок

--- app/fix_me.py
+++ app/fix_me.py
@@ -1,22 +1,22 @@
 def max_number(a, b):
-    if a > b:
-        return a
-    else:
-        return b
+	if a > b:
+		return a
+	else:
+		return b
 
 
 def multiply(a, b):
-    return a * b
+	return a * b
 
 
 class FixMe:
-    def __init__(self, name):
-        self.name = name
+	def __init__(self, name):
+		self.name = name
 
-    def zero_function(self, x):
-        if x > 0:
-            print("positive")
-        elif x < 0:
-            print("negative")
-        else:
-            print("zero")
+	def zero_function(self, x):
+		if x > 0:
+			print("positive")
+		elif x < 0:
+			print("negative")
+		else:
+			print("zero")
